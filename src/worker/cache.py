ab28834399c0368dd13a5264d1dab1cc1366fe34
Chris de Graaf <me@cdg.dev>
2021-05-15 23:32:38 -0300

    Swap out Redis cache for DynamoDB

--- src/worker/cache.py
+++ src/worker/cache.py
@@ -1,44 +1,36 @@
 import logging
 import os
-import time
 
-from typing import Optional, cast
+from typing import Optional
 
-from redis import Redis
+import boto3
 
-JOB_TIMEOUT = int(os.getenv("JOB_TIMEOUT", "3600"))
-PREFIX = "video"
+VIDEOS = boto3.resource("dynamodb").Table(os.environ["VIDEOS_TABLE_NAME"])
 PROGRESS = "progress"
-REDIS = Redis(os.getenv("REDIS_HOST", "localhost"))
 
 
 def get_video(score: int) -> Optional[str]:
     """Get the URL to a previously uploaded video of `score`."""
-    _wait(score)
-    val = cast(Optional[bytes], REDIS.get(f"{PREFIX}:{score}"))
-    return val.decode() if val else None
+    item = VIDEOS.get_item(Key={"id": score}).get("Item")
+    if not item:
+        return None
+    if item.get(PROGRESS):
+        # TOOD: Exit but jump back here in a little while.
+        logging.info(f"Video for score {score} is in progress...")
+        raise Exception("TODO")
+    return item["url"]
 
 
 def set_video(score: int, url: str) -> None:
     """Set the URL of `score` to `url`."""
-    REDIS.set(f"{PREFIX}:{score}", url)
-    set_video_progress(score, False)
+    VIDEOS.put_item(Item={"id": score, "url": url})
 
 
 def set_video_progress(score: int, status: bool) -> None:
     """Mark `score` as being currently or no longer in progress."""
-    key = f"{PREFIX}:{PROGRESS}:{score}"
-    if status:
-        REDIS.set(key, "true", ex=JOB_TIMEOUT)
+    item = VIDEOS.get_item(Key={"id": score}).get("Item")
+    if item:
+        item[PROGRESS] = status
     else:
-        REDIS.delete(key)
-
-
-def _wait(score: int) -> None:
-    """Wait until a progress marker for `score` no longer exists."""
-    key = f"{PREFIX}:{PROGRESS}:{score}"
-    if not REDIS.get(key):
-        return
-    logging.info("Waiting for in-progress video...")
-    while REDIS.get(key):
-        time.sleep(1)
+        item = {"id": score, PROGRESS: True}
+    VIDEOS.put_item(Item=item)
