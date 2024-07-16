ef4429e6b5e039b0573301f467024b5a04ee350d
MasterIO02 <masterio@issou.best>
2024-07-16 09:33:49 +0200

    Update websocket

--- src/websocket/ws.py
+++ src/websocket/ws.py
@@ -7,15 +7,17 @@ from src.worker.cache import is_render_active, set_render_id
 fmt = "%(asctime)s %(levelname)s: %(message)s"
 logging.basicConfig(level=logging.INFO, format=fmt)
 
+sio = socketio.Client(reconnection=True)
 
-def on_connect() -> None:
+@sio.event
+def connect() -> None:
     logging.info("o!rdr websocket connected.")
 
-
-def on_disconnect() -> None:
+@sio.event
+def disconnect() -> None:
     logging.error("o!rdr websocket disconnected.")
 
-
+@sio.on("render_done_json")
 def on_render_done(data: Any) -> None:
     try:
         if is_render_active(data["renderID"]):
@@ -26,7 +28,7 @@ def on_render_done(data: Any) -> None:
             f"o!rdr websocket: error handling render finished - data: {data}"
         )
 
-
+@sio.on("render_failed_json")
 def on_render_failed(data: Any) -> None:
     try:
         if is_render_active(data["renderID"]):
@@ -37,11 +39,6 @@ def on_render_failed(data: Any) -> None:
             f"o!rdr websocket: error handling render failed - data: {data}"
         )
 
-
 def main() -> None:
-    sio = socketio.Client()
-    sio.on("connect", on_connect)
-    sio.on("disconnect", on_disconnect)
-    sio.on("render_done_json", on_render_done)
-    sio.on("render_failed_json", on_render_failed)
-    sio.connect("https://ordr-ws.issou.best")
+    sio.connect("https://apis.issou.best", socketio_path="/ordr/ws")
+    sio.wait()
